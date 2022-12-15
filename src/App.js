6fac4e0420ca0b7b20ee1dfc365c1389003ce833
wtto0 <74681245+wtto0@users.noreply.github.com>
2022-12-15 17:13:37 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 // import { BrowserRouter as Router } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Axios } from "axios";
+import { useState } from "react";
 
 import styles from "./App.module.css";
-
 import useWindow from "./components/hooks/useWindow";
 import Navbar from "./components/layout/fixed/Navbar";
 import Footer from "./components/layout/fixed/Footer";
@@ -11,31 +12,34 @@ import Login from "./components/boxes/Login";
 import Register from "./components/boxes/Register";
 import AddEntry from "./components/boxes/addEntry/AddEntry";
 import AddTransf from "./components/boxes/addTransf/AddTransf";
-import { Axios } from "axios";
-import { useState } from "react";
 import Message from "./components/elements/banners/Message";
-
 
 function App() {
   const hook = useWindow();
-  const [logged, setLogged] = useState(true)
+  const [logged, setLogged] = useState(true);
 
   return (
     <Router>
       <Navbar hook={hook} customClass={logged} />
       {/* <Message type="error" msg="Mensagem de teste"/> */}
-      <Message type={hook.typeMsg} msg={hook.msg}/>
+      <Message type={hook.typeMsg} msg={hook.msg} />
       <div
         className={`${styles.contentBlock} ${
           hook.blurVisivel && styles.active
         }`}
       />
-      <AnimatedRoutes hook={hook} className={styles.animatedRoutes}/>
+      <AnimatedRoutes hook={hook} className={styles.animatedRoutes} />
 
       <Footer />
       <div id="modals">
         <Login show={hook.loginVisivel} />
-        <Register show={hook.registerVisivel} setShow={hook.displayRegister} logged={logged} setLogged={setLogged} hook={hook}/>
+        <Register
+          show={hook.registerVisivel}
+          setShow={hook.displayRegister}
+          logged={logged}
+          setLogged={setLogged}
+          hook={hook}
+        />
         <AddEntry
           expense={false}
           show={hook.revenueVisivel}
